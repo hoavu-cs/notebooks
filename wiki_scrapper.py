d0ce3aa8cb2a56509acb5f9f64b1600ac8669c8d
hoavu-cs <hvutrong.gmail.com>
2023-10-04 08:44:34 -0700

    wiki gpt

--- wiki_scrapper.py
+++ wiki_scrapper.py
@@ -1,23 +1,23 @@
-import wikipediaapi
+import wikipedia
 import requests
 import json
 import csv
 import time
-
+import pandas as pd
 # Initialize Wikipedia API wrapper
-wiki_wiki = wikipediaapi.Wikipedia("vi")
+wikipedia.set_lang("vi")
 
 # Initialize counter
 counter = 0
 
 # Open a CSV file to write summaries
-with open('article_summaries.csv', 'w', encoding='utf-8', newline='') as f:
+with open('article_summaries_2.csv', 'w', encoding='utf-8', newline='') as f:
     writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL)
     
     # Write the header
     writer.writerow(["Title", "Summary"])
     
-    while counter < 5:
+    while counter < 1000:
         try:
             # Fetch a random Vietnamese Wikipedia article
             random_article_url = "https://vi.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json"
@@ -28,15 +28,18 @@ with open('article_summaries.csv', 'w', encoding='utf-8', newline='') as f:
             title = data['query']['random'][0]['title']
 
             # Fetch article
-            page = wiki_wiki.page(title)
-
-            # Check if page exists
-            if not page.exists():
+            try:
+                summary = wikipedia.summary(title)
+            except wikipedia.exceptions.DisambiguationError as e:
+                print(f"Disambiguation page found for {title}. Skipping.")
+                continue
+            except wikipedia.exceptions.PageError as e:
                 print(f"Article {title} does not exist. Skipping.")
                 continue
 
             # Write plain text summary to CSV file
-            writer.writerow([page.title, page.summary])
+            # Write plain text summary to CSV file
+            writer.writerow([f'{title}', f'{summary}'])
 
             # Increment counter
             counter += 1
@@ -49,3 +52,6 @@ with open('article_summaries.csv', 'w', encoding='utf-8', newline='') as f:
 
         except Exception as e:
             print(f"An error occurred: {e}")
+
+df = pd.read_csv('article_summaries.csv')
+print(df.head())
