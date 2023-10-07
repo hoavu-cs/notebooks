a30250945c44b9cb6e3bd41e1b955bfd55a61e93
hoavu-cs <hvutrong.gmail.com>
2023-10-07 16:12:58 -0700

    wikipedia

--- wiki_scrapper.py
+++ wiki_scrapper.py
@@ -4,54 +4,50 @@ import json
 import csv
 import time
 import pandas as pd
-# Initialize Wikipedia API wrapper
-wikipedia.set_lang("en")
 
-# Initialize counter
-counter = 0
+# Constants
+wikipedia.set_lang("vi")
+SIZE = 500000
+BATCH_SIZE = 100  # number of articles to fetch in one request
+cache = set()
 
-# Open a CSV file to write summaries
-with open('article_summaries_3.csv', 'w', encoding='utf-8', newline='') as f:
-    writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL)
-    
-    # Write the header
-    writer.writerow(["Title", "Summary"])
-    
-    while counter < 2:
-        try:
-            # Fetch a random Vietnamese Wikipedia article
-            random_article_url = "https://vi.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json"
-            response = requests.get(random_article_url)
-            data = json.loads(response.text)
+def fetch_random_articles(batch_size):
+    random_article_url = f"https://vi.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit={batch_size}&format=json"
+    response = requests.get(random_article_url)
+    data = json.loads(response.text)
+    return [item['title'] for item in data['query']['random']]
 
-            # Extract article title
-            title = data['query']['random'][0]['title']
+def fetch_article_content(title):
+    try:
+        content = wikipedia.page(title).summary
+        return (title, content)
+    except wikipedia.exceptions.DisambiguationError:
+        print(f"Disambiguation page found for {title}. Skipping.")
+    except wikipedia.exceptions.PageError:
+        print(f"Article {title} does not exist. Skipping.")
+    return None
 
-            # Fetch article
-            try:
-                summary = wikipedia.summary(title)
-            except wikipedia.exceptions.DisambiguationError as e:
-                print(f"Disambiguation page found for {title}. Skipping.")
-                continue
-            except wikipedia.exceptions.PageError as e:
-                print(f"Article {title} does not exist. Skipping.")
-                continue
+def main():
+    with open('article_summaries.csv', 'w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["Title", "Summary"])
 
-            # Write plain text summary to CSV file
-            # Write plain text summary to CSV file
-            writer.writerow([f'{title}', f'{summary}'])
+        total_batches = SIZE // BATCH_SIZE
 
-            # Increment counter
-            counter += 1
+        for batch_number in range(total_batches):
+            titles = fetch_random_articles(BATCH_SIZE)
+            articles = []
+            for title in titles:
+                if title not in cache:
+                    cache.add(title)
+                    articles.append(fetch_article_content(title))
+            for article in articles:
+                if article:
+                    writer.writerow(article)
+            print(f"Fetched {(batch_number + 1) * BATCH_SIZE} articles")
 
-            # Print status
-            print(f"Fetched {counter} articles")
-
-            # Add delay to avoid rate-limiting
-            time.sleep(1)
-
-        except Exception as e:
-            print(f"An error occurred: {e}")
+if __name__ == "__main__":
+    main()
 
 df = pd.read_csv('article_summaries.csv')
 print(df.head())
